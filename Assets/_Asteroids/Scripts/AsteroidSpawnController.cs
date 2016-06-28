0fbbdaa51315d8bde63e768a497343b5c785b110
SergeRichard <serge.richard@rogers.com>
2016-06-28 19:53:11 -0300

    Asteroids now spawn at start of game.
    Asteroids now spawn at start of game as well as move and rotate. They
    also are moved to the other end of screen when passing boundaries.

---
+++ Assets/_Asteroids/Scripts/AsteroidSpawnController.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class AsteroidSpawnController : MonoBehaviour {
+
+	public GameObject[] AsteroidSpawnPoints;
+
+	public GameObject Asteroid1;
+	public GameObject Asteroid2;
+	public GameObject Asteroid3;
+
+	private List<GameObject> asteroids = new List<GameObject>();
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	public void SpawnAsteroids(int numOfAsteroids) {
+		for (int t = 0; t < numOfAsteroids; t++) {
+			GameObject asteroid = (GameObject)Instantiate (Asteroid1, AsteroidSpawnPoints [Random.Range (0, 13)].transform.position, Quaternion.identity);
+			asteroids.Add (asteroid);
+		}
+
+	}
+
+	// Update is called once per frame
+	void Update () {	
+
+		foreach (var asteroid in asteroids) {
+			CheckBoundaries (asteroid);
+		}
+	}
+	void CheckBoundaries(GameObject asteroid) {
+		Rigidbody rigidBody = asteroid.GetComponent<Rigidbody> ();
+
+		if (asteroid.transform.position.x > 7) {
+			rigidBody.MovePosition(new Vector3(-7,asteroid.transform.position.y, asteroid.transform.position.z));
+		}
+		if (asteroid.transform.position.x < -7) {
+			rigidBody.MovePosition(new Vector3(7,asteroid.transform.position.y, asteroid.transform.position.z));
+		}
+		if (asteroid.transform.position.z > 5.3f) {
+			rigidBody.MovePosition(new Vector3(asteroid.transform.position.x, asteroid.transform.position.y, -5.3f));
+		}
+		if (asteroid.transform.position.z < -5.3f) {
+			rigidBody.MovePosition(new Vector3(asteroid.transform.position.x, asteroid.transform.position.y, 5.3f));
+		}
+	}
+}
---
+++ Assets/_Asteroids/Scripts/GameManager.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+
+	public AsteroidSpawnController AsteroidSpawnController;
+
+	// Use this for initialization
+	void Start () {
+		AsteroidSpawnController.SpawnAsteroids (4);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
