6958c7129ba90722eb8bb7db7d2bc6b123c4733d
SergeRichard <serge.richard@rogers.com>
2016-07-09 09:35:28 -0300

    Added UFO.
    UFO now spawns every 8 seconds from the left or right side of the screen
    and moves across the screen. UFO does not shoot bullets yet.

---
+++ Assets/_Asteroids/Scripts/UFO.cs
@@ -0,0 +1,99 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UFO : MonoBehaviour {
+	public float Speed;
+	public float TimeToChangeDirection;
+
+	private float startTimeMoving;
+
+	public GameObject UFOExplosion;
+
+
+	void Start ()
+	{
+		startTimeMoving = Time.time;
+		ChangeDirection ();
+	}
+
+	void Update() {
+		CheckBoundaries ();
+		if (Time.time - startTimeMoving > TimeToChangeDirection) {
+			startTimeMoving = Time.time;
+			ChangeDirection ();
+		}
+
+	}
+	void ChangeDirection() {
+		if (UfoController.UfoDirection == UfoController.UfoDirections.Left) {
+			MovingLeft ();
+
+		} else {
+			MovingRight ();
+
+		}
+
+
+
+	}
+	void MovingLeft() {
+		switch (Random.Range (0, 3)) {
+		case 0:
+			// Diagonal up, right
+			GetComponent<Rigidbody> ().velocity = new Vector3 (-Speed, 0, Speed);
+
+			break;
+		case 1:
+			// Diagonal down, right
+			GetComponent<Rigidbody> ().velocity = new Vector3 (-Speed, 0, -Speed);
+			break;
+
+		case 2:
+			GetComponent<Rigidbody>().velocity = -transform.right * Speed;
+			break;
+
+		}
+
+	}
+	void MovingRight() {
+		switch (Random.Range (0, 3)) {
+		case 0:
+			// Diagonal up, right
+			GetComponent<Rigidbody> ().velocity = new Vector3 (Speed, 0, Speed);
+
+			break;
+		case 1:
+			// Diagonal down, right
+			GetComponent<Rigidbody> ().velocity = new Vector3 (Speed, 0, -Speed);
+			break;
+
+		case 2:
+			GetComponent<Rigidbody>().velocity = transform.right * Speed;
+			break;
+
+		}
+	}
+	void CheckBoundaries() {
+		Rigidbody rigidBody = GetComponent<Rigidbody> ();
+
+		if (transform.position.x > 7) {
+			rigidBody.MovePosition(new Vector3(-7,transform.position.y, transform.position.z));
+		}
+		if (transform.position.x < -7) {
+			rigidBody.MovePosition(new Vector3(7,transform.position.y, transform.position.z));
+		}
+		if (transform.position.z > 5.3f) {
+			rigidBody.MovePosition(new Vector3(transform.position.x, transform.position.y, -5.3f));
+		}
+		if (transform.position.z < -5.3f) {
+			rigidBody.MovePosition(new Vector3(transform.position.x, transform.position.y, 5.3f));
+		}
+	}
+	void OnCollisionEnter(Collision collision) {
+		UfoController.StartTimeSpawn = Time.time;
+
+		Instantiate (UFOExplosion, transform.position, Quaternion.identity);
+		Destroy (gameObject);
+	}
+}
+
---
+++ Assets/_Asteroids/Scripts/UfoController.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UfoController : MonoBehaviour {
+	public float TimeToSpawn = 8f;
+	public enum UfoDirections { Right, Left};
+	public static UfoDirections UfoDirection;
+	public static float StartTimeSpawn;
+	public GameObject UFO;
+
+
+	void Start ()
+	{
+		StartTimeSpawn = Time.time;
+
+		SetMoveDirection ();
+	}
+	
+	void Update() {
+		if (transform.childCount == 0) {
+			SetMoveDirection ();
+			if (Time.time - StartTimeSpawn >= TimeToSpawn) {
+
+				float vertical = Random.Range (-6.5f, 6.5f);
+
+				if (UfoDirection == UfoDirections.Left) {
+					// Instantiate the UFO to the right side of the screen
+					GameObject go = (GameObject)Instantiate(UFO,new Vector3(6.5f,0,vertical), Quaternion.Euler(new Vector3(90,0,0)));
+					go.transform.parent = transform;
+				} else {
+					// Instantiate the UFO to the right side of the screen
+					GameObject go = (GameObject)Instantiate(UFO,new Vector3(-6.5f,0,vertical), Quaternion.Euler(new Vector3(90,0,0)));
+					go.transform.parent = transform;
+				}
+			}
+
+		}
+
+	}
+	void SetMoveDirection() {
+		if (Random.Range (0, 2) == 0) {
+			UfoDirection = UfoDirections.Left;
+		} else {
+			UfoDirection = UfoDirections.Right;
+		}
+
+	}
+}
